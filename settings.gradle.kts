24b22f951ba4b42651b2e438f995ee5b94171b52
我不是罗大锤 <1696547463@qq.com>
2023-03-31 14:15:33 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
+}
+
+rootProject.name = "CivilAviationManagerSystem"
+
