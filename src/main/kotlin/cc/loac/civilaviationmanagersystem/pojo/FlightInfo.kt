ed76e5e9a99bb48bd017749a8749111fde9451e6
我不是罗大锤 <1696547463@qq.com>
2023-04-17 13:22:31 +0800

    定义实体类

---
+++ src/main/kotlin/cc/loac/civilaviationmanagersystem/pojo/FlightInfo.kt
@@ -0,0 +1,25 @@
+package cc.loac.civilaviationmanagersystem.pojo
+
+import java.util.Date
+
+/**
+ * 航班信息实体类
+ */
+data class FlightInfo(
+    // 航班号
+    var id: String,
+    // 出发地
+    var from: String,
+    // 目的地
+    var to: String,
+    // 起飞时间
+    var departureTime: Date,
+    // 到达时间
+    var arrivalTime: Date,
+    // 飞行时间
+    var flightTime: Int,
+    // 人数
+    var persons: Int,
+    // 票价
+    var price: Double
+)
